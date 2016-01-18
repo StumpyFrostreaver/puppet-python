0d6b9261cf0e98ec631a7b5f16a15d4cf7e107f3
Stumpy Frostreaver <Stumpy@clarmitage.ca>
2016-01-18 09:52:35 -0500

    Update params.pp
    Added 34 and 35 to Red Hat valid versions...

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,7 +12,7 @@ class python::params {
   $manage_gunicorn        = true
   $provider               = undef
   $valid_versions = $::osfamily ? {
-    'RedHat' => ['3','27','33'],
+    'RedHat' => ['3','27','33', '34', '35'],
     'Debian' => ['3', '3.3', '2.7'],
     'Suse'   => [],
   }
